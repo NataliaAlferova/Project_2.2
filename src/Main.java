7ecbf004e0615845d6db145fda8f4fabc7070e92
Natalia Alferova <natalia@alferova.me>
2021-10-29 21:21:03 +0200

    Version 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        double startBalance = 90;
+        double incoming = 1500;
+        int bonus;
+        if (incoming >= 1000) {
+            bonus = (int) incoming / 10
+        } ;
+        else {
+            bonus = 0;
+        }
+
+    }
